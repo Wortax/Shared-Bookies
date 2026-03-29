80b16bfef42b82a2b59f5848d48e7ca95991e483
Wortax <69532627+Wortax@users.noreply.github.com>
2026-03-29 11:19:39 +0200

    Update ModEntry.cs

--- ModEntry.cs
+++ ModEntry.cs
@@ -10,6 +10,7 @@ namespace SharedBookies
     {
         private static readonly List<string> PowerBooks_list = new()
         {
+            //Vanilla game Books
             "Book_AnimalCatalogue",
             "Book_Mystery",
             "Book_Bombs",
@@ -29,7 +30,44 @@ namespace SharedBookies
             "Book_Artifact",
             "Book_Speed",
             "Book_Speed2",
-            "Book_Woodcutting"
+            "Book_Woodcutting",
+
+            //Spiderbuttons ExtraBooks
+            "Spiderbuttons.ButtonsExtraBooks_Book_Carols",
+            "Spiderbuttons.ButtonsExtraBooks_Book_Luck",
+            "Spiderbuttons.ButtonsExtraBooks_Book_ExtraGifts",
+            "Spiderbuttons.ButtonsExtraBooks_Book_TreesIgnoreSeason",
+            "Spiderbuttons.ButtonsExtraBooks_Book_ArtisanMachines",
+            "Spiderbuttons.ButtonsExtraBooks_Book_GiantCrops",
+            "Spiderbuttons.ButtonsExtraBooks_Book_Popularity",
+            "Spiderbuttons.ButtonsExtraBooks_Book_Optimization",
+            "Spiderbuttons.ButtonsExtraBooks_Book_BusDriving",
+            "Spiderbuttons.ButtonsExtraBooks_Book_QiNotebook",
+            "Spiderbuttons.ButtonsExtraBooks_Book_PetGifts",
+            "Spiderbuttons.ButtonsExtraBooks_Book_CheatCodes",
+            "Spiderbuttons.ButtonsExtraBooks_Book_JunimoScrap",
+            "Spiderbuttons.ButtonsExtraBooks_Book_Carols",
+            "Spiderbuttons.ButtonsExtraBooks_Book_Coffee",
+            "Spiderbuttons.ButtonsExtraBooks_Book_Sketchbook",
+
+
+            // jeWel More Books
+            "jeWel.MoreBooks_Book_Bomb",
+            "jeWel.MoreBooks_Book_God",
+            "jeWel.MoreBooks_Book_Attack",
+            "jeWel.MoreBooks_Book_Green_Tea",
+            "jeWel.MoreBooks_Book_Milk",
+            "jeWel.MoreBooks_Book_Garbage_Can",
+            "jeWel.MoreBooks_Book_Dagger",
+            "jeWel.MoreBooks_Book_Ore_Catalogue",
+            "jeWel.MoreBooks_Book_Rice",
+            "jeWel.MoreBooks_Book_Speed",
+            "jeWel.MoreBooks_Book_Carpenter_Catalogue",
+            "jeWel.MoreBooks_Book_Lewis_shorts",
+            "jeWel.MoreBooks_Book_Luck",
+            "jeWel.MoreBooks_Book_Immunity",
+            "jeWel.MoreBooks_Book_Magnet",
+            "jeWel.MoreBooks_Book_Fishing"
         };
 
         public override void Entry(IModHelper helper)
@@ -49,11 +87,11 @@ namespace SharedBookies
 
             var data = new SyncData();
 
-            foreach (string stat in PowerBooks_list)
+            foreach (string book in PowerBooks_list)
             {
-                if (Game1.player.stats.Get(stat) > 0)
+                if (Game1.player.stats.Get(book) > 0)
                 {
-                    data.PowerBooks.Add(stat);
+                    data.PowerBooks.Add(book);
                 }
             }
 
@@ -73,11 +111,11 @@ namespace SharedBookies
 
             int powersAdded = 0;
 
-            foreach (string stat in data.PowerBooks)
+            foreach (string book in data.PowerBooks)
             {
-                if (Game1.player.stats.Get(stat) == 0)
+                if (Game1.player.stats.Get(book) == 0)
                 {
-                    Game1.player.stats.Set(stat, 1);
+                    Game1.player.stats.Set(book, 1);
                     powersAdded++;
                 }
             }
